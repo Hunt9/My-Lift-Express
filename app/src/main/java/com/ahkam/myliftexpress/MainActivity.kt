5e59a5609f697b7ed0e0859aa9971d836f276b07
Hunt9 <champthe20@gmail.com>
2021-11-11 01:23:56 +0500

    Splash Done

--- app/src/main/java/com/ahkam/myliftexpress/MainActivity.kt
+++ app/src/main/java/com/ahkam/myliftexpress/MainActivity.kt
@@ -3,10 +3,52 @@ package com.ahkam.myliftexpress
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.ahkam.myliftexpress.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setSplashAnimation()
+        startTimer()
     }
+
+    private fun setSplashAnimation() {
+        val myLiftAnimation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.slide_in_left
+        )
+
+        val expressAnimation = AnimationUtils.loadAnimation(
+            this,
+            R.anim.slide_in_right
+        )
+
+        binding.myLift.startAnimation(myLiftAnimation)
+        myLiftAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(arg0: Animation) {}
+            override fun onAnimationRepeat(arg0: Animation) {}
+            override fun onAnimationEnd(arg0: Animation) {
+                binding.express.visibility = View.VISIBLE
+                binding.express.startAnimation(expressAnimation)
+            }
+        })
+
+
+    }
+
+    private fun startTimer() {
+        Handler(Looper.getMainLooper()).postDelayed({ Toast.makeText(this,"Yeah",Toast.LENGTH_LONG).show() }, 2000)
+    }
+
 }
